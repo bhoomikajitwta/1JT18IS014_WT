6be1b3b8e564bea68492b7a497c208f6d19ae61a
bhoomikajitwta <83061143+bhoomikajitwta@users.noreply.github.com>
2021-07-01 22:09:55 +0530

    Add files via upload

--- Program 7/prog7.php
+++ Program 7/prog7.php
@@ -1,1 +1,28 @@
-
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="refresh" content="1" />
+<style>
+p {
+color: white;
+font-size: 60px;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+}
+body {
+background-color: green;
+}
+</style>
+<body>
+<p> 
+<?php 
+echo "The server time is ". date(" h: i : s A"). "<br>";
+echo "Today is ". date("Y/m/d"). "<br>" ;
+echo "Today is ". date("Y.m.d"). "<br>" ;
+echo "Today is ". date("Y-m-d"). "<br>" ;
+echo "Today is ". date("1") ;
+?> </p>
+</head>
+</html>
